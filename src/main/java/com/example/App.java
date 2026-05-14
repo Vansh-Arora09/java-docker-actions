4bfc87f12360f38a6b24f0da69bdf519f1d81585
vansh <va325746@gmail.com>
2026-05-15 00:40:17 +0530

    done

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,7 @@
-echo "package com.example; public class App { public static void main(String[] args) { System.out.println(\"Hello LPU!\"); } }"
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions!");
+    }
+}
